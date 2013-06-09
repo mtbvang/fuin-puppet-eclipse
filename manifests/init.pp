c88125f11da33de472439a26098ddffc3f1e9bb4
Roman Voitenko <r00mka@gmail.com>
2013-06-10 01:26:15 +0200

    eclipse: removed hardcodes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,22 @@
-#TODO M2: remove hardcodes + menu
-class eclipse {
+#TODO M2: add menu
+class eclipse inherits settings {
   #todo -> move to module
     archive { 'eclipse':
 	ensure => present,
-	url    => 'http://eclipse.mirror.triple-it.nl/technology/epp/downloads/release/juno/SR2/eclipse-jee-juno-SR2-linux-gtk-x86_64.tar.gz',
-	target => '/home/dev',
+	url    => "$downloadurl/eclipse-jee-juno-SR2-linux-gtk-x86_64.tar.gz",
+	target =>  "$userhomedir/",
 	checksum => false,
 	timeout => '900'
     }
 
-    file { '/home/dev/eclipse-4.2.2':
+    file { "$userhomedir/eclipse-4.2.2":
 	ensure => 'link',
-	target => '/home/dev/eclipse',
+	target => "$userhomedir/eclipse",
     }
 
-    exec { "sed -i 's|vmargs|vm/home/dev/oracle-jdk-7u21|g' /home/dev/eclipse-4.2.2/eclipse.ini":
+    exec { "sed -i 's|vmargs|vm$userhomedir/$jdk|g' $userhomedir/eclipse-4.2.2/eclipse.ini":
 	path => [ '/usr/local/bin', '/usr/bin', '/bin', ],
-	onlyif => "/bin/grep -E 'vmargs' '/home/dev/eclipse-4.2.2/eclipse.ini'",
+	onlyif => "/bin/grep -E 'vmargs' $userhomedir/eclipse-4.2.2/eclipse.ini",
     }
 
 
