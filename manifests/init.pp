728f63b5aec6ae15ce6f44b55bc7e073a217c748
Roman Voitenko <r00mka@gmail.com>
2013-06-12 10:43:45 +0200

    added feature_add function

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,6 +8,8 @@ class eclipse inherits settings {
 	timeout => '900',
     }
 
+    Exec { path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" }
+
     file { "$userdevdir/eclipse-4.2.2":
 	ensure => 'link',
 	target => "$userdevdir/eclipse",
@@ -26,6 +28,18 @@ class eclipse inherits settings {
 	path   => "$userdevdir/eclipse-4.2.2/eclipse.ini",
 	content => template("eclipse/eclipse.ini.erb")
     }
+
+    define feature_add {
+	exec { "adding feature $name":
+	    cwd     => "$userdevdir/eclipse",
+	    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$userdevdir/eclipse",
+	    command => "./eclipse -application org.eclipse.equinox.p2.director -noSplash  -repository $name"
+	}
+    }
+    
+    feature_add  { $eclipse_features : }
+    
+    
 
 }
 
